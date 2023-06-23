7a742505fa0a1eb970dbc504ecd881c7157d5963
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-06-23 20:47:28 +0300

    new changes made

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -2,7 +2,7 @@ import React, { useState, Component } from 'react';
 import RocketCore from './RocketCore';
 
 export function FunctionalRocket() {
-  const [initialLaunchTime] = useState(Date.now());
+  const [ initialLaunchTime ] = useState(Date.now());
 
   return <RocketCore initialLaunchTime={initialLaunchTime} />;
 }
