f8c3bbdbe37933dd787a071df7a17e64fb8d4af2
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-06-24 12:01:16 +0300

    new state created for dark mode

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import '../styles/_app.scss';
 
 function App() {
+  const [ darkModeOpen, isDarkModeOpen ] = useState(false);
   return (
     <div className="app">
       <div className="level">
@@ -13,7 +15,8 @@ function App() {
 
         {/* --The button that should toggle dark mode-- */}
         <button className="app__dark-mode-btn icon level-right">
-          <FontAwesomeIcon icon={faMoon} />
+          <FontAwesomeIcon onClick={() => isDarkModeOpen(!darkModeOpen)} icon={ darkModeOpen ? faSun : faMoon} 
+          />
         </button>
 
       </div>
