dcad624cce074e78db89fb9f21401189d075dd01
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-06-24 07:40:21 +0300

    changed the numbers

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/RocketCore.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/RocketCore.js
@@ -3,7 +3,7 @@ import '../styles/_rocket.scss';
 
 const SECONDS_TO_TAKEOFF = 5;
 const MS_TO_TAKEOFF = SECONDS_TO_TAKEOFF * 1000;
-const FINAL_POSITION_BOTTOM_VAL = 'calc(400px)';
+const FINAL_POSITION_BOTTOM_VAL = 'calc(1200px)';
 
 function timeToPositionPercent(startTime) {
   const now = Date.now();
