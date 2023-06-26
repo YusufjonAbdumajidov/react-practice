875a9bbeb3ce4b3098577754022b5f6a2f070bb0
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-06-26 21:22:19 +0300

    new changes

--- chatter/src/App.js
+++ chatter/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import CoreLayout from './layouts/CoreLayout';
 
-export default function App() {
-  return (
-    <CoreLayout />
-  );
-}
+// export default function App() {
+//   return (
+//     <CoreLayout />
+//   );
+// }
